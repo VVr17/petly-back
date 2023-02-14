bbb1a48dbcc5a19c370b6aa423a8886b8a99c989
Vira Voronova <vera.voronova@hotmail.com>
2023-02-14 09:46:00 +0100

    base swagger spec added

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.get("/", (_, res) => {
-  res.send(`API is running`);
-});
-
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((_, res) => {
   res.status(404).json("Use api on route not found");
