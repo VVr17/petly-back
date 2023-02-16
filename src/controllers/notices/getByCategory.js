950027769a531c79fccc0830275e81cb768e23ad
AnnaSidiakina <102148925+AnnaSidiakina@users.noreply.github.com>
2023-02-16 16:14:02 +0100

    Schema/Model/Notice

--- src/controllers/notices/getByCategory.js
+++ src/controllers/notices/getByCategory.js
@@ -1,3 +1,8 @@
+const { Notice } = require("../../models/noticeModel");
 export const getByCategoryController = async (req, res) => {
+  const { page = 1, limit = 8 } = req.query;
+  const { gategoryName } = req.params.gategoryName;
+
+  // const data = await
   res.status(200).json("get by cat");
 };
--- src/schemas/noticeSchema.js
+++ src/schemas/noticeSchema.js
@@ -1,3 +1,12 @@
 import Joi from "joi";
 
-export const noticeSchema = Joi.object({});
+export const noticeSchema = Joi.object({
+  title: Joi.string().min(2).max(48).required(),
+  name: Joi.string().min(2).max(16),
+  breed: Joi.string().min(2).max(24),
+  birthdate: Joi.date(),
+  location: Joi.string(),
+  comments: Joi.string().min(8).max(120).required(),
+  price: Joi.number().greater(0).required(),
+  favorite: Joi.bool(),
+});
