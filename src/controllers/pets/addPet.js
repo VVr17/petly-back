27a7f349478bbfc79ca0d6207abf5eed96dcbb04
Nick Zheliba <nikolaj.jeliba@gmail.com>
2023-02-17 13:52:06 +0200

    Pet Schema

--- src/controllers/pets/addPet.js
+++ src/controllers/pets/addPet.js
@@ -1,17 +1,16 @@
-// import { nanoid } from "nanoid"; добавить либу и сгенеровать айди
 import { Pet } from "../../models/petModel.js";
 import { setSuccessResponse } from "../../helpers/setResponse.js";
 // import createError from "http-errors";
 
 export const addPetController = async (req, res) => {
-  const { name, birthday, breed, photoURL, comments } = req.body;
+  const { name, birthDate, breed, photoURL, comments } = req.body;
 
   //  TODO: add owner id
 
   const savedPet = await Pet.create({
-    // _id: nanoid(),
+    // ownerId: 
     name,
-    birthday,
+    birthDate,
     breed,
     // photoURL:
     comments,
