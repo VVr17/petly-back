8da99675b519bd9423e47a72116089e19ba82216
Mykola Opalenko <mykola.opalenko@gmail.com>
2023-02-18 14:30:36 +0200

    favorite

--- src/controllers/notices/addToFavorite.js
+++ src/controllers/notices/addToFavorite.js
@@ -1,3 +1,24 @@
+import { User } from "../../models/userModel.js"
+import { setSuccessResponse } from "../../helpers/setResponse.js";
+
 export const addToFavoriteController = async (req, res) => {
-  res.status(200).json("add to favorite");
+   const { noticeId } = req.params
+   const { userId } = req.user;
+   const user = await User.findById(userId);
+
+   const checkNotice = user.favoriteNotices.includes(noticeId);
+
+   if (checkNotice) {
+      return res.json({
+         code: 409,
+         message: 'Notice is already added.',
+       });
+   }
+   
+   user.favoriteNotices.push(noticeId);
+   await user.save();
+
+
+   res.json(setSuccessResponse(200, user.favoriteNotices));
+
 };
--- src/controllers/notices/getFavorites.js
+++ src/controllers/notices/getFavorites.js
@@ -1,3 +1,35 @@
+import { User } from "../../models/userModel.js"
+import { Notice } from "../../models/noticeModel.js"
+import { setSuccessResponse } from "../../helpers/setResponse.js";
+
+
 export const getFavoritesController = async (req, res) => {
-  res.status(200).json("get favorites");
+   const { userId } = req.user;
+   const user = await User.findById(userId)
+   const idArray = user.favoriteNotices.map(favorite => { return favorite.toString() });  
+   const favorites = await Notice.find({ _id: idArray });
+
+
+   if (!favorites) {       
+      const error = new Error();
+      throw error(404, 'You have no any favorite notices');    
+  }
+  res.status(200).json(favorites);
 };
+
+
+
+
+
+
+// import {User} from "../../models/userModel.js"
+// import { setSuccessResponse } from "../../helpers/setResponse.js";
+
+
+// export const getFavoritesController = async (req, res) => {
+//    const { userId } = req.user;
+//    const UserNotice = await User.findById(userId)
+//    if (UserNotice) {
+//       res.json(setSuccessResponse(200, UserNotice.favoriteNotices));
+//    }
+// };
