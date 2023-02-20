9d94afd2ac65322880f5c9c2f59af39ff9bc8549
Vira Voronova <vera.voronova@hotmail.com>
2023-02-20 18:22:46 +0100

    validation for notices added

--- src/schemas/noticeSchema.js
+++ src/schemas/noticeSchema.js
@@ -29,14 +29,22 @@ export const noticeSchema = Joi.object({
 export const noticeSellSchema = Joi.object({
   title: Joi.string().min(2).max(48).required(),
   photoURL: Joi.string().uri().required(),
-  name: Joi.string().min(2).max(16).required(),
-  breed: Joi.string().min(2).max(24).required(),
+  name: Joi.string()
+    .min(2)
+    .max(16)
+    .regex(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/, "Only letters can be accepted")
+    .required(),
+  breed: Joi.string()
+    .min(2)
+    .max(24)
+    .regex(/^[a-zA-Z]+$/, "Only letters can be accepted")
+    .required(),
   sex: Joi.string().required(),
-  birthDate: Joi.date().format("DD-MM-YYYY").required(),
+  birthDate: Joi.date().less(Date.now()).format("DD.MM.YYYY").required(),
   location: Joi.string()
     .regex(
-      /^\s*(?:\w+\s*,\s*){1,}(?:\w+\s*)$/,
-      "at least two words separated by string"
+      /^[a-zA-Z]+(?:-[a-zA-Z]+)*,\s*[a-zA-Z\s]+$/,
+      "Should be at least two words separated by string"
     )
     .required(),
   comments: Joi.string().min(8).max(120).required(),
