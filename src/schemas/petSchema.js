1ea4178a438ddbd06009aa874b8dabdc6d042664
Vira Voronova <vera.voronova@hotmail.com>
2023-02-20 14:04:14 +0100

    pet schema joi url added

--- src/schemas/petSchema.js
+++ src/schemas/petSchema.js
@@ -1,8 +1,9 @@
 import Joi from "joi";
 
 export const petSchema = Joi.object({
-    name: Joi.string().min(2).max(16).alphanum().required(),
-    birthDate: Joi.string().required(),
-    breed: Joi.string().min(2).max(16).alphanum().required(),
-    comments: Joi.string().min(8).max(120),
+  name: Joi.string().min(2).max(16).alphanum().required(),
+  birthDate: Joi.string().required(),
+  breed: Joi.string().min(2).max(16).alphanum().required(),
+  comments: Joi.string().min(8).max(120),
+  photoURL: Joi.string().uri().required(),
 });
