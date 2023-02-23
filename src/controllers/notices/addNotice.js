65b3f69dd7846d95452269b9c882c97b4335d6a3
Vira Voronova <vera.voronova@hotmail.com>
2023-02-23 08:15:21 +0100

    validation notice temp removed

--- src/controllers/notices/addNotice.js
+++ src/controllers/notices/addNotice.js
@@ -7,13 +7,15 @@ export const addNoticeController = async (req, res) => {
   const { categoryName } = req.params;
   const { body } = req;
   const { userId } = req.user;
+  console.log("body", body);
+  console.log("req?.file?.path", req?.file?.path);
 
-  const data = await Notice.create({
-    ...body,
-    photoURL: req?.file?.path,
-    category: categoryName,
-    owner: userId,
-  });
-
-  res.json(setSuccessResponse(200, data));
+  // const data = await Notice.create({
+  //   ...body,
+  //   photoURL: req?.file?.path,
+  //   category: categoryName,
+  //   owner: userId,
+  // });
+  res.json("its ok");
+  // res.json(setSuccessResponse(200, data));
 };
--- src/schemas/noticeSchema.js
+++ src/schemas/noticeSchema.js
@@ -3,51 +3,13 @@ import JoiDate from "@joi/date";
 const Joi = BaseJoi.extend(JoiDate);
 
 export const noticeSchema = Joi.object({
-  title: Joi.string().min(2).max(48).required(),
+  title: Joi.string().min(2).max(48),
   imageFile: Joi.object({
-    originalname: Joi.string().required(),
+    originalname: Joi.string(),
     fieldname: Joi.string(),
     encoding: Joi.string(),
     mimetype: Joi.string(),
-    path: Joi.string().required(),
-    size: Joi.number(),
-    filename: Joi.string(),
-  }).required(),
-  name: Joi.string()
-    .min(2)
-    .max(16)
-    .regex(
-      /^[a-zA-Zа-яА-Я]+(?: [a-zA-Zа-яА-Я]+)*$/,
-      "Only letters can be accepted"
-    )
-    .required(),
-  breed: Joi.string()
-    .min(2)
-    .max(24)
-    .regex(
-      /^[a-zA-Zа-яА-Я]+(?: [a-zA-Zа-яА-Я]+)*$/,
-      "Only letters can be accepted"
-    )
-    .required(),
-  sex: Joi.string().valid("male", "female").required(),
-  birthDate: Joi.date().less(Date.now()).format("DD.MM.YYYY").required(),
-  location: Joi.string()
-    .regex(
-      /^[a-zA-Zа-яА-Я]+(?:-[a-zA-Zа-яА-Я]+)*,\s*[a-zA-Zа-яА-Я\s]+$/,
-      "Should be at least two words separated by string"
-    )
-    .required(),
-  comments: Joi.string().min(8).max(200).required(),
-});
-
-export const noticeSellSchema = Joi.object({
-  title: Joi.string().min(2).max(48).required(),
-  imageFile: Joi.object({
-    originalname: Joi.string().required(),
-    fieldname: Joi.string(),
-    encoding: Joi.string(),
-    mimetype: Joi.string(),
-    path: Joi.string().required(),
+    path: Joi.string(),
     size: Joi.number(),
     filename: Joi.string(),
   }),
@@ -57,26 +19,56 @@ export const noticeSellSchema = Joi.object({
     .regex(
       /^[a-zA-Zа-яА-Я]+(?: [a-zA-Zа-яА-Я]+)*$/,
       "Only letters can be accepted"
-    )
-    .required(),
+    ),
   breed: Joi.string()
     .min(2)
     .max(24)
     .regex(
       /^[a-zA-Zа-яА-Я]+(?: [a-zA-Zа-яА-Я]+)*$/,
       "Only letters can be accepted"
-    )
-    .required(),
-  sex: Joi.string().valid("male", "female").required(),
-  birthDate: Joi.date().less(Date.now()).format("DD.MM.YYYY").required(),
-  location: Joi.string()
-    .regex(
-      /^[a-zA-Zа-яА-Я]+(?:-[a-zA-Zа-яА-Я]+)*,\s*[a-zA-Zа-яА-Я\s]+$/,
-      "Should be at least two words separated by string"
-    )
-    .required(),
-  comments: Joi.string().min(8).max(200).required(),
-  price: Joi.number().greater(0).required().required(),
+    ),
+  sex: Joi.string().valid("male", "female"),
+  birthDate: Joi.date().less(Date.now()).format("DD.MM.YYYY"),
+  location: Joi.string().regex(
+    /^[a-zA-Zа-яА-Я]+(?:-[a-zA-Zа-яА-Я]+)*,\s*[a-zA-Zа-яА-Я\s]+$/,
+    "Should be at least two words separated by string"
+  ),
+  comments: Joi.string().min(8).max(200),
 });
 
-// Joi.array().items(Joi.string().required(), Joi.string().required());
+export const noticeSellSchema = Joi.object({
+  title: Joi.string().min(2).max(48),
+  imageFile: Joi.object({
+    originalname: Joi.string(),
+    fieldname: Joi.string(),
+    encoding: Joi.string(),
+    mimetype: Joi.string(),
+    path: Joi.string(),
+    size: Joi.number(),
+    filename: Joi.string(),
+  }),
+  name: Joi.string()
+    .min(2)
+    .max(16)
+    .regex(
+      /^[a-zA-Zа-яА-Я]+(?: [a-zA-Zа-яА-Я]+)*$/,
+      "Only letters can be accepted"
+    ),
+  breed: Joi.string()
+    .min(2)
+    .max(24)
+    .regex(
+      /^[a-zA-Zа-яА-Я]+(?: [a-zA-Zа-яА-Я]+)*$/,
+      "Only letters can be accepted"
+    ),
+  sex: Joi.string().valid("male", "female"),
+  birthDate: Joi.date().less(Date.now()).format("DD.MM.YYYY"),
+  location: Joi.string().regex(
+    /^[a-zA-Zа-яА-Я]+(?:-[a-zA-Zа-яА-Я]+)*,\s*[a-zA-Zа-яА-Я\s]+$/,
+    "Should be at least two words separated by string"
+  ),
+  comments: Joi.string().min(8).max(200),
+  price: Joi.number().greater(0),
+});
+
+// Joi.array().items(Joi.string(), Joi.string());
