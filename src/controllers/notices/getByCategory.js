e3b66f5270ce9a16ae1aadb55ec875f36358c8ee
Vira Voronova <vera.voronova@hotmail.com>
2023-02-26 09:31:29 +0100

    merged fixed

--- src/controllers/notices/getByCategory.js
+++ src/controllers/notices/getByCategory.js
@@ -3,11 +3,7 @@ import { Notice } from "../../models/noticeModel.js";
 import createError from "http-errors";
 
 export const getByCategoryController = async (req, res) => {
-<<<<<<< HEAD
-  const { page = 1, limit } = req.query;
-=======
   const { page = 1, limit, search } = req.query;
->>>>>>> 6971270c506863fed61c6cc8d1c69d31eca7491d
   const { categoryName } = req.params;
   const skip = (page - 1) * limit;
   const totalItems = await Notice.find({ category: categoryName }).count();
