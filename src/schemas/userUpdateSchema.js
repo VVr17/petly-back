d11c454601e823edd11eb3aaa23112f4b8c6acd8
Vira Voronova <vera.voronova@hotmail.com>
2023-02-28 18:09:49 +0100

    user update fixed

--- src/schemas/userUpdateSchema.js
+++ src/schemas/userUpdateSchema.js
@@ -8,7 +8,6 @@ export const userUpdateSchema = Joi.object({
       /^(?=.{1,63}$)(?=.{2,}@)[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
       "Please enter a valid email address"
     )
-    .required()
     .min(12)
     .max(63),
   city: Joi.string()
