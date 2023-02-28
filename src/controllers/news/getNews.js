87dde4032af2ba4ed1e0f3352e585955289cfe79
AnnaSidiakina <102148925+AnnaSidiakina@users.noreply.github.com>
2023-02-28 11:19:48 +0100

    sortNewsByDate

--- src/controllers/news/getNews.js
+++ src/controllers/news/getNews.js
@@ -2,6 +2,7 @@ import { setSuccessResponse } from "../../helpers/setResponse.js";
 import { News } from "../../models/newsModel.js";
 
 export const getNewsController = async (req, res) => {
-  const news = await News.find();
+
+  const news = await News.find().sort({ date: "descending" });
   res.json(setSuccessResponse(200, news));
 };
